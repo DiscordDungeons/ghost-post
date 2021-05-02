9822fcf367cda19ca466b3ea6627a1313aafc6bf
Sven65 <Sven65@users.noreply.github.com>
2021-05-02 15:36:01 +0200

    Small improvements

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import * as core from '@actions/core'
 import { PostParams } from '@tryghost/admin-api'
 import { addPost } from './addPost'
 
-const getParams = (): PostParams => ({
+const params: PostParams = {
 	title: core.getInput('POST_TITLE', { required: true }),
 	html: core.getInput('POST_HTML', { required: true }),
 	custom_excerpt: core.getInput('POST_EXCERPT'),
 	tags: core.getInput('POST_TAGS').split(','),
-})
-
+}
 
 async function run(): Promise<void> {
 	try {
-		const post = await addPost(getParams())
+		const post = await addPost(params)
 
 		core.setOutput('postId', post.id)
 	} catch (error) {
